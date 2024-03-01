e6d2cd661d8511ae72e7fb56695c929aebb453b1
Bauke <me@bauke.xyz>
2024-03-02 00:05:17 +0100

    Initial commit!

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      flake-utils,
+      nixpkgs,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = import ./shell.nix { inherit pkgs; };
+      }
+    );
+}
